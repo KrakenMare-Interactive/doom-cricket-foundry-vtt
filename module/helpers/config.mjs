853e20bd116519c85a319c72b4214a380637df28
William Bidstrup <williambidstrup@192.168.20.13>
2021-10-04 19:36:40 +1030

    Initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const BOILERPLATE = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ BOILERPLATE.abilities = {
+  "str": "DOOMCRICKET.AbilityStr",
+  "dex": "DOOMCRICKET.AbilityDex",
+  "con": "DOOMCRICKET.AbilityCon",
+  "int": "DOOMCRICKET.AbilityInt",
+  "wis": "DOOMCRICKET.AbilityWis",
+  "cha": "DOOMCRICKET.AbilityCha"
+};
+
+BOILERPLATE.abilityAbbreviations = {
+  "str": "DOOMCRICKET.AbilityStrAbbr",
+  "dex": "DOOMCRICKET.AbilityDexAbbr",
+  "con": "DOOMCRICKET.AbilityConAbbr",
+  "int": "DOOMCRICKET.AbilityIntAbbr",
+  "wis": "DOOMCRICKET.AbilityWisAbbr",
+  "cha": "DOOMCRICKET.AbilityChaAbbr"
+};
